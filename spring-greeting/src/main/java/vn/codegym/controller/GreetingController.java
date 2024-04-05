641fcd1976ea896f01e8365630dd6cd626bb49d8
Ngo Hoang <ngohoang8320@gmail.com>
2024-04-05 13:36:22 +0700

    Add practice about the greeting

---
+++ spring-greeting/src/main/java/vn/codegym/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package vn.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String Greeting(@RequestParam String name,
+                           Model model) {
+        model.addAttribute("name",
+                name);
+        return "index";
+    }
+}
